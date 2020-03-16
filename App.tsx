dbc5075f94b71c5d2bff8ba5abaef7a7bdc2edd9
YKStudio <ashn01@naver.com>
2020-03-16 19:19:35 -0400

    Initial app

---
+++ App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons';
+
+import Home from './screens/Home';
+import Settings from './screens/Settings';
+
+
+/*
+ *  createStackNavigator is a function that returns an object containing 2 properties: Screen and Navigator. 
+ *  Both of them are React components used for configuring the navigator. 
+ *  The Navigator should contain Screen elements as its children to define the configuration for routes.
+*/
+// const Stack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+/*
+ *  NavigationContainer is a component which manages our navigation tree and contains the navigation state. 
+ *  This component must wrap all navigators structure. 
+ *  Usually, we'd render this component at the root of our app, which is usually the component exported from App.js.
+*/
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} options={{title:'Todo'}}/>
+        <Tab.Screen name="Settings" component={Settings}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/Settings.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function Settings({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text>This is Settings!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
