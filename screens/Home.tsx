4544d4f0fe00090b7febbdc19a773ed1ca9975e1
YKStudio <ashn01@naver.com>
2020-04-06 13:10:01 -0400

    Modified screen name

---
+++ screens/Home.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import { StyleSheet} from 'react-native';
+import {Container, Drawer} from 'native-base';
+
+import HeaderBar from './HeaderBar'
+import TodoLists from './TodoLists'
+import FooterBar from './FooterBar'
+
+export default function Home() {
+    return (
+        <Container>
+              <HeaderBar/>
+              <TodoLists/>
+              <FooterBar/>
+        </Container>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 64,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
